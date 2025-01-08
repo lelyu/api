79a6243237f00b7d47a5c5a7827c828394db1ae5
lyule <lyule@bc.edu>
2025-01-07 21:43:39 -0500

    getters and setters for item

---
+++ src/main/java/com/ttc/api/model/Item.java
@@ -0,0 +1,41 @@
+package com/ttc.api.model;
+import jakarta.persistence.*;
+
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String appName;
+    private String appDescription;
+    private String appURI;
+
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getAppName() {
+        return appName;
+    }
+    public void setAppName(String appName) {
+        this.appName = appName;
+    }
+    public String getAppDescription() {
+        return appDescription;
+    }
+    public void setAppDescription(String appDescription) {
+        this.appDescription = appDescription;
+    }
+    public String getAppURI() {
+        return appURI;
+    }
+    public void setAppURI(String appURI) {
+        this.appURI = appURI;
+    }
+    
+}
